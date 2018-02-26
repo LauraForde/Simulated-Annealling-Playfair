f1567f6df84b1cb665425d2d1e11b5ce4c252fcc
Rebecca <rebecca.kane@outlook.com>
2018-02-26 11:35:08 +0000

    Starting Matrix, might refactor into PlayfairImpl if matrix stuff simpler than expected

---
+++ AIProject/src/ie/gmit/sw/ai/PlayfairMatrix.java
@@ -0,0 +1,28 @@
+package ie.gmit.sw.ai;
+
+public class PlayfairMatrix {
+	
+	// Based on Step 2 of PlayFair in spec
+	
+	/* Rule 1	Digraph Letters in different rows and columns
+		Create a “box” inside the matrix with each diagraph letter as a corner
+		read off the letter at the opposite corner of the same row 
+		-> cipher(B, P)={matrix[row(B)][col(P)], matrix[row(P)][col(B)]}.
+		Reverse to decrypt.
+	*/
+	
+	/* Rule 2	Digraph Letters in Same Row 
+	 	Replace any letters that appear on the same row with the letters to their immediate right
+	 	Wrap around the matrix if necessary. 
+	 	Decrypt by replacing cipher-text letters the with letters on their immediate left.
+	*/
+	
+	/* Rule 3	Digraph Letters in Same Column
+	 	Replace any letters that appear on the same column with the letters immediately below
+		Wrap back around the top of the column if necessary
+	 	Decrypt by replacing cipher text letters the with letters immediately above. 
+	*/
+	
+	
+
+}
