78ef52f2cc0a250a4cab3685f4ef9f39e4402655
Rebecca <rebecca.kane@outlook.com>
2018-04-02 18:46:44 +0100

    Tidied up menu, added length of time to decrypt

--- AIProject/src/ie/gmit/sw/ai/CipherBreaker.java
+++ AIProject/src/ie/gmit/sw/ai/CipherBreaker.java
@@ -8,59 +8,88 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-// Runner class - doesn't actually do anything, just calls other classes/methods
+
 public class CipherBreaker {
 	static PlayfairImpl pf = new PlayfairImpl();
 	static FilePreparer fp = new FilePreparer();
 	static Scanner input = new Scanner(System.in);
+	static String filename;
 
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Enter Option: \n1. Decrypt a file\n2. Exit program");
+		System.out.println("Enter Option: \n1. Decrypt File (known key)\n2. Decrypt File (unknown key)\n3. Exit program");
 		menu(); // Run the menu method
+		
 	}
 
 	// Handles getting choice from the user
 	public static void menu() {
 		int choice = input.nextInt();
+		String primedText;
 		switch (choice) {
 	        case 1:
-	        	runProg(); // User wants to decrypt file, execute runProg() method
-	            break;
+	        	primedText = askFile();
+	        	knownKey(primedText);
+	        	break;
 	        case 2:
+	        	runSimulatedAnnealling(); // User wants to decrypt file, execute runProg() method
+	        	break;
+	        case 3:
 	            System.out.println("- Terminated Program -"); // User wants to quit
 	            break;
 	        default:
-	            System.out.println("Invalid option! Please choose an option from the menu below. \n1. Decrypt a file\n2. Exit program");
+	            System.out.println("Invalid option! Please choose an option from the menu below. \n1. Decrypt File (known key)\n2. Decrypt File (unknown key)\n3. Exit program");
 	            menu(); // Handling possibility of user entering invalid option, just call menu() until choice is valid
 	    }
 	
 	}
 	
-	public static void runProg() {
+	public static void runSimulatedAnnealling() {
 		SimulatedAnnealling sa = new SimulatedAnnealling();
 		String primedText = askFile();
 		String[] digraphs = pf.makeDigraphs(primedText);
-		
+		String decrypted;
+
 		System.out.print("Please note this program could take a long time to find the correct key.\nEnter a time limit in seconds (the program will terminate and output the best key found up to that point).\nEnter 0 if you are happy to let the program run as long as required, up to a 10 minute cut off.\nTime Limit: ");
 		int timeLimit = input.nextInt();
 
 		try {
 			System.out.println("Decrypting... ");
-			sa.decrypt(digraphs, timeLimit);
+			decrypted = sa.decrypt(digraphs, timeLimit);
+			fp.writeFile(filename, decrypted);
 			
 		} catch (IOException e) {
 			System.out.println("Unable to decrypt. Please choose an option from the menu.");
-			menu();
 		}
+		System.out.println("\nEnter Option: \n1. Decrypt File (known key)\n2. Decrypt File (unknown key)\n3. Exit program");
+		menu();
+	}
+	
+	public static void knownKey(String primedText) {
+		long start = System.currentTimeMillis();
+		String[] digraphs = pf.makeDigraphs(primedText);
+		String decrypted;
+		System.out.print("Enter key to decrypt with: ");
+		String key = input.next(); 
+		key = key.toUpperCase().replaceAll("[^A-Za-z0-9 ]", ""); // Replace all non letter chars
+		if(key.length() != 25) {
+			System.out.print("Key must be 25 characters in length and contain only letters! ");
+			knownKey(primedText);
+		}
+		decrypted = pf.pfDecrypt(key, digraphs);
+		fp.writeFile(filename, decrypted);
+		
+		System.out.println("\nDecrypted in " + ((System.currentTimeMillis() - start) / 1000.0) + " seconds. \n\nEnter Option: \n1. Decrypt File (known key)\n2. Decrypt File (unknown key)\n3. Exit program");
+		menu();
+	
 	}
 	
 	public static String askFile() {
-		System.out.println("Please enter name of .txt file to decrypt:");
-		String filename = input.next();
+		System.out.print("\nPlease enter name of .txt file to decrypt. Make sure the file is in the same directory as this project, e.g. Documents.\nFile name:");
+		filename = input.next();
 		
-		if(!filename.contains(".txt")) {
-			filename += ".txt";
+		if(!filename.contains(".txt")) { // If the user didn't enter an extension add it on
+			filename ="..\\" + filename + ".txt"; // ..\\ Move 2 dirs back to same folder project is in
 		}
 		
 		Path path = FileSystems.getDefault().getPath(filename);
--- AIProject/src/ie/gmit/sw/ai/FilePreparer.java
+++ AIProject/src/ie/gmit/sw/ai/FilePreparer.java
@@ -24,9 +24,23 @@ public class FilePreparer {
 			System.out.println("Oops! File not found.");
 		    CipherBreaker.askFile();
 		}
-
 		return fileString;
-		
+	}
+	
+	public void writeFile(String infilename, String decrypted) {
+		PrintWriter out;
+		// Using ..\\ to go 2 dirs back (dir containing project, desktop if project is on desktop, etc), need to get rid of ..\\ from the passed in file name too
+		String outfilename = "..\\Decrypted-" + infilename.replace("..\\", ""); 
+		// Don't need to worry about extension for in or outfilename, added to filename in CipherBreaker class
+		System.out.println("Writing to " + outfilename.replace("..\\", ""));
+		try {
+			out = new PrintWriter(outfilename);
+			out.println(decrypted);
+			out.close();
+			System.out.println(outfilename.replace("..\\", "") + " saved.");
+		} catch (FileNotFoundException e) {
+			System.out.println("Oops! Error writing to file.");
+		}
 	}
 	
 	// Quadgram stuff - method generates a Map of String/Double pairs and returns completed map
