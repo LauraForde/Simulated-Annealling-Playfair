77ceddcfff8dfb6f4a27d85c119829b94179824a
Rebecca <rebecca.kane@outlook.com>
2018-04-04 18:08:24 +0100

    SA seems to be working..!!!

--- AIProject/src/ie/gmit/sw/ai/SimulatedAnnealling.java
+++ AIProject/src/ie/gmit/sw/ai/SimulatedAnnealling.java
@@ -1,6 +1,7 @@
 package ie.gmit.sw.ai;
 
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.util.Map;
 import java.util.Random;
 
@@ -13,89 +14,74 @@ public class SimulatedAnnealling {
 	
 	private int temperature;
 	private int transitions = 50000;
-//	private String key;
-//	private double fitness;
-//	private String decrypted;
 
 	Map<String, Double> quadgramMap;
 
 	public String decrypt(String encryptedText) throws Exception {	
 		//temperature = (int)((10 + 0.087 * (encryptedText.length() - 84)));
-		temperature = 15;
+		temperature = 20;
 		//temperature = 10;
 		setQuadgramMap(ngh.getQuad());
-		String key = k.shuffle(); // Generate random 25 letter key called parent
-		String decrypted =  pf.decryptPF(key, encryptedText); // Decrypt with that key
-		double keyFitness = scoreFitness(decrypted); // Score the fitness of the key
+		String parentKey = k.shuffle(); // Generate random 25 letter key called parent
+		String decrypted =  pf.decryptPF(parentKey, encryptedText); // Decrypt with that key
+		double parentFitness = scoreFitness(decrypted); // Score the fitness of the key
 		String tmpDcrypt;
-		System.out.println("Init key: " + key + ", score " + keyFitness + ", decrypt: " + decrypted + "\n");
-		Random r = new Random();
-		String bestKey = key;
-		double bestFitness = keyFitness;
+		System.out.println("Init key: " + parentKey + ", score " + parentFitness + ", decrypt: " + decrypted + "\n");
+		Random r = new SecureRandom();
 		
 		for(int i = temperature; i > 0; i--) {
 			long start = System.currentTimeMillis();
 			for (int j = transitions; j > 0; j--) {
-				String childKey = k.modifyKey(key); // Make small change
+				String childKey = k.modifyKey(parentKey); // Make small change
 				tmpDcrypt = pf.decryptPF(childKey, encryptedText);
 				double childFitness = scoreFitness(tmpDcrypt); // Score the fitness of the new key
 				
-				//System.out.println("Testing " + childKey + " with score " + childFitness + ". Decrypted: " + tmpDcrypt);
-				
-				double delta = childFitness - keyFitness;	
-				if(delta > 0) {									// if the delta is over 0 this key is better
-					key = childKey;
-					keyFitness = childFitness;
+				double delta = childFitness - parentFitness;	
+				if(delta > 0) { // Child / new key better
+					parentKey = childKey; // Set parent = child
+					parentFitness = childFitness; // Set parent fitness = child fitness
 
 				} else  {
-					if(Math.exp(-delta/i) > 0.5) { // prevent getting stuck
-						key = childKey;
-						keyFitness = childFitness;
+					// Equation will return double between 0 and 1 - closer to 1 = better, delta/i means will be less likely to take worse key at lower temp(i)
+					if(Math.exp(delta/i) > r.nextDouble()) {
+						parentKey = childKey; // Set parent = child
+						parentFitness = childFitness; // Set parent fitness = child fitness
 					}
 				}
 			
-				if(keyFitness > bestFitness) {
-					bestFitness = keyFitness;
-					bestKey = key;
-					
-				}
 			} // End transitions
 			//Thread.sleep(10); // Sleeping because it runs too fast to output the score properly... just for display, sleep fixes issue
 			//System.out.println("Temp " + i +  ". Current key: " + bestKey + " with score " + bestFitness + ". Decrypted: " + bestDec.substring(0, 20) + "..." + ". Time: " + ((System.currentTimeMillis() - start) / 1000.0) + "s");	
-			System.out.println("Temp " + i +  ". Current key: " + key + " with score " + keyFitness + ". Decrypted: " + pf.decryptPF(bestKey, encryptedText).substring(0, 20) + "..." + ". Time: " + ((System.currentTimeMillis() - start) / 1000.0) + "s \n");	
+			System.out.println("Temp " + i +  ". Current key: " + parentKey + " with score " + parentFitness + ". Decrypted: " + pf.decryptPF(parentKey, encryptedText).substring(0, 20) + "..." + ". Time: " + ((System.currentTimeMillis() - start) / 1000.0) + "s \n");	
 		} // End temp
 		//Thread.sleep(5); // Same here
-		//System.out.println("Completed at temp " + temperature + " with " + transitions + " transitions each. Total tests: " + (temperature*transitions) + "\nBest Key: " + bestKey + ", with score: " + bestFitness + ". Decrypted: " + bestDec.substring(0, 100) + "...");
-		System.out.println("Completed at temp " + temperature + " with " + transitions + " transitions each. Total tests: " + (temperature*transitions) + "\nBest Key: " + key + ", with score: " + keyFitness + ". Decrypted: " + decrypted.substring(0, 100) + "...\n");
+		System.out.println("Completed at temp " + temperature + " with " + transitions + " transitions each. Total tests: " + (temperature*transitions) + "\nBest Key: " + parentKey + ", with score: " + parentFitness + ". Decrypted: " + pf.decryptPF(parentKey, encryptedText).substring(0, 20) + "...");
 		return decrypted;
 	}
 	
 	// Score fitness
-		public double scoreFitness(String decrypted) throws IOException {
-			double score = 0;
-			long total = 4224127912L;
-			
-			int limit = decrypted.length();
-			if(limit > 43) // Not going to test entire text file, will work on 10 quadgrams
-				limit = 43; // 13 = index of last letter in 10th quadgram (10-13)
-			
-			for (int index = 0; index <= limit - 4; index ++) {
-				Double occurences = (Double) quadgramMap.get(decrypted.substring(index, index + 4));
-				//System.out.print(decrypted.substring(index, index + 4) + "\t\t" + quadgramMap.get(decrypted.substring(index, index + 4)) + "\t\t" + occurences);
-				if (occurences != null) {
-					score += Math.log10(occurences/total);
-				}
+	public double scoreFitness(String decrypted) throws IOException {
+		double score = 0;
+		long total = 4224127912L; // Total number of possible quadgrams = 4,224,127,912.
+		
+		for(int i = 0; i < decrypted.length() - 4; i++) { // For each char in the decrypted text
+			Double occurences = quadgramMap.get(decrypted.substring(i, i+4)); // Get the number of occurences of the current gram (chars i to i+4) of decrypted text
+			if(occurences == null) { // If the gram has no occurences in 4grams.txt
+				occurences = 1.0; // Set it to one occurance (for the sake of occurences / total -> can't divide by 0)
 			}
-			return score;
+			score += Math.log10((double) occurences / total); // Add the probability (occurences / total)
 		}
-		
-		public Map<String, Double> getQuadgramMap() {
-			return quadgramMap;
-		}
+		return score;
+	}
+	
+	
+	// Gets & Sets for the map of quadgrams
+	public Map<String, Double> getQuadgramMap() {
+		return quadgramMap;
+	}
 
-		public void setQuadgramMap(Map<String, Double> quadgramMap) {
-			this.quadgramMap = quadgramMap;
-		}
+	public void setQuadgramMap(Map<String, Double> quadgramMap) {
+		this.quadgramMap = quadgramMap;
+	}
 		
-
 }
